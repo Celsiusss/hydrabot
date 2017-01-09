e0ab3659605c9018bb1af968b66701c944cfc790
BuildTools <unconfigured@null.spigotmc.org>
2017-01-09 22:30:22 +0100

    Added *working* queue system

--- bot.js
+++ bot.js
@@ -6,34 +6,65 @@ bot.on('ready', () => {
 	console.log(`Logged in as ${bot.user.username}!`);
 });
 
+let queue = [];
+
 bot.on("message", msg => {
 	const prefix ="!";
-
+	
 	if (!msg.content.startsWith(prefix)) return;
 	if (msg.author.bot) return;
 	
 	if (msg.content.startsWith(prefix + "help")) {
-		msg.reply("Say !yt play YT_URL to play music. Say !yt stop to stop.");
+		msg.sendMessage("Say !play YT_URL to play music. Say !stop to stop.");
 	}
 	
 	if (msg.content.startsWith(prefix + "play")) {
 		
-		const args = msg.content.split(" ");
+		let args = msg.content.split(" ");
 		
 		if (args.length > 1) {
-			const streamOptions = {seek: 0, volume: 1};
-			const voiceChannel = msg.member.voiceChannel;
+			if (args[1].startsWith("http://www.youtube.com/watch?v=") ||
+				args[1].startsWith("https://www.youtube.com/watch?v=")) {
+				
+				const streamOptions = {seek: 0, volume: 1};
+				const voiceChannel = msg.member.voiceChannel;
+				
+				if (queue.length <= 0) {
+					voiceChannel.join().then(connection => {
+						queue.push(args[1]);
 
-			voiceChannel.join().then(connection => {
+						play(queue[0], connection);
 
-				const stream = ytdl(args[2], {filter: 'audioonly'}); //PLay :D
-				const dispatcher = connection.playStream(stream, streamOptions);
-			}).catch(console.error);
+						function play(streamurl, connection) {
 
+							console.log('Playing stream ' + streamurl);
+                            const stream = ytdl(streamurl, {filter: 'audioonly'}); //Play :D
+                            const dispatcher = connection.playStream(stream, streamOptions);
+
+                            dispatcher.on("end", () => {
+                                queue.shift();
+                                if (queue.length > 0) {
+                                    play(queue[0], connection);
+                                } else {
+                                    connection.disconnect();
+                                }
+
+                                console.log("Stream ended");
+                            });
+                        }
+						
+					}).catch(console.error);
+				} else {
+					queue.push(args[1]);
+				}
+			} else {
+				msg.reply("That URL is not valid, it must be a full Youtube URL.")
+			}
 		} else {
 			msg.reply("I need a Youtube URL for that.");
 		}
 	}
+	
 	if (msg.content.startsWith(prefix + "stop")) {
 		const voiceChannel = msg.member.voiceChannel;
 		voiceChannel.join().then(connection => {
@@ -41,10 +72,26 @@ bot.on("message", msg => {
 		});
 	}
 	
+	if (msg.content.startsWith(prefix + "queue")) {
+
+		if (queue.length > 0) {
+            msg.channel.sendMessage(queue.join("\n"))
+                .then(msg => console.log(`Sent message: ${msg.content}`))
+                .catch(console.error);
+        } else {
+            msg.channel.sendMessage("Queue is empty!")
+                .then(msg => console.log(`Sent message: ${msg.content}`))
+                .catch(console.error);
+		}
+	}
+
+    if (msg.content.startsWith(prefix + "clear")) {
+		queue = [];
+		msg.channel.sendMessage("Queue cleared!")
+			.then(msg => console.log(`Sent message: ${msg.content}`))
+            .catch(console.error);
+    }
+	
 });
 
-bot.on("end", () => {
-	bot.msg
-});
-
-bot.login('MjY2ODU4MjM4Mzc5NTU2ODg1.C1HblQ.006LRe5q8dtLbRiZtq6vX7Vu-Jo');
+bot.login('MjY2ODU4MjM4Mzc5NTU2ODg1.C1QK5A.fO2Xh4ccWD_6cCd8ef6DIzcLQp4');
