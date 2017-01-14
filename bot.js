89a1f67cedce1bcd96271e68ac014d973586f907
BuildTools <unconfigured@null.spigotmc.org>
2017-01-14 01:06:25 +0100

    Did some stuff

--- bot.js
+++ bot.js
@@ -3,12 +3,13 @@ const ytdl = require('ytdl-core');
 const search = new require("youtube-search");
 const bot = new Discord.Client();
 const fs = new require("fs");
+const probe = require('pmx').probe();
 var cleverbot = require("cleverbot.io"),
 		clever = new cleverbot("jp6wu9XZbYdoICmo", "54jV1VcMNxGQyc2cdKUFUpjkPVo3bTr2");
 
 bot.on('ready', () => {
 	bot.user.setGame(".help");
-	console.log(`Logged in as ${bot.user.username}!`);
+	console.log(ct() + `Logged in as ${bot.user.username}!`);
 });
 
 let searchOptions = {
@@ -28,6 +29,18 @@ let timer = false;
 //let guildID = msg.guild.id;
 //queue[guildID] = [];
 
+let counter = probe.counter({
+	name: "Streams"
+});
+let guilds = probe.metric({
+	name: "Guilds",
+	value: bot.guilds.size
+});
+let population = probe.metric({
+	name: "Population",
+	value: 5
+});
+
 bot.on("message", (msg) => {
 	
 	const prefix =".";
@@ -35,7 +48,7 @@ bot.on("message", (msg) => {
 	let id = bot.user.id;
 	let clevername = new RegExp(`^<@!?${id}>`);
 	
-	if (msg.content.startsWith(prefix)) {} else if (clevername.test(msg.content)) {console.log("Yes")} else return;
+	if (msg.content.startsWith(prefix)) {} else if (clevername.test(msg.content)) {} else return;
 	if (msg.author.bot) return;
 
 	let guildID = msg.guild.id;
@@ -45,12 +58,14 @@ bot.on("message", (msg) => {
 		queue[msg.guild.id] = [];
 	}
 
+	console.log(ct() + msg.author.username + " (" + msg.author.id + ") issued command: " + msg.content);
+
 	//Command cooldown
 	function cooldown() {
 
 		if (timer == true) {
 			msg.channel.sendMessage("You're sending commands too quickly!")
-				.then(msg => console.log(`Sent message: ${msg.content}`))
+				.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 				.catch(console.error);
 			return;
 		}
@@ -74,7 +89,7 @@ bot.on("message", (msg) => {
 				"@Hydra - Talk to Hydra!\n" +
 			"\n" +
 			"Hydra Discord channel:  https://discord.gg/UcZc3uX")
-			.then(msg => console.log(`Sent message: ${msg.content}`))
+			.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 			.catch(console.error);
 	}
 
@@ -89,7 +104,7 @@ bot.on("message", (msg) => {
 				!args[1].startsWith("https://www.youtube.com/watch?v=")) {
 
 				search(msg.content.substring(prefix.length + "play".length), searchOptions, (err, results) => {
-					if (err) return console.log(err);
+					if (err) return console.log(ct() + err);
 
 					args[1] = results[0].link;
 					start();
@@ -112,23 +127,23 @@ bot.on("message", (msg) => {
 						}).catch(console.error);
 					} catch (e) { //I should not do it this way, but meh
 						msg.channel.sendMessage("Join a voice channel before jamming")
-							.then(msg => console.log(`Sent message: ${msg.content}`))
+							.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 							.catch(console.error);
 					}
 				} else {
 					ytdl.getInfo(args[1], (err, info) => {
 						if (err) {
-							console.log(err);
+							console.log(ct() + err);
 							msg.channel.sendMessage("Error adding song, please try again.")
-								.then(msg => console.log(`Sent message: ${msg.content}`))
+								.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 								.catch(console.error);
 							return;
 						}
 						queue[msg.guild.id].push([args[1], info.title + " (" + timestamp(info.length_seconds) + ")"]);
 
-						console.log("Added url to queue " + queue[msg.guild.id][0]);
+						console.log(ct() + "Added url to queue " + queue[msg.guild.id][0]);
 						msg.channel.sendMessage("Song added to queue: " + queue[msg.guild.id][queue[msg.guild.id].length - 1][1])
-								.then(msg => console.log(`Sent message: ${msg.content}`))
+								.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 								.catch(console.error);
 					});
 				}
@@ -150,11 +165,11 @@ bot.on("message", (msg) => {
 				queuelist += i + ". " + queue[msg.guild.id][i][1] + "\n";
 			}
             msg.channel.sendMessage(queuelist)
-                .then(msg => console.log(`Sent message: ${msg.content}`))
+                .then(msg => console.log(ct() + `Sent message: ${msg.content}`))
                 .catch(console.error);
 		} else {
 			msg.channel.sendMessage("No videos in queue.")
-					.then(msg => console.log(`Sent message: ${msg.content}`))
+					.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 					.catch(console.error);
 		}
 	}
@@ -174,7 +189,7 @@ bot.on("message", (msg) => {
 
 		msg.channel.sendMessage("" +
 				"Hydra is online on " + count + " servers!")
-				.then(msg => console.log(`Sent message: ${msg.content}`))
+				.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 				.catch(console.error);
 
 		dbots.postBotStats(botid, stats);
@@ -190,11 +205,11 @@ bot.on("message", (msg) => {
 		clever.setNick(msg.author.username);
 
 		clever.create(function (err, session) {
-			if (err) console.log(err);
+			if (err) console.log(ct() + err);
 			clever.ask(string, function (err, response) {
-				if (err) console.log(err);
+				if (err) console.log(ct() + err);
 				msg.channel.sendMessage(response)
-						.then(msg => console.log(`Sent message: ${msg.content}`))
+						.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 						.catch(console.error);
 			});
 		});
@@ -204,7 +219,7 @@ bot.on("message", (msg) => {
 		if (msg.content.startsWith(prefix + "skip")) {
 
 			msg.channel.sendMessage("Song skipped!")
-					.then(message => console.log(`Sent message: ${message.content}`))
+					.then(message => console.log(ct() + `Sent message: ${message.content}`))
 					.catch(console.error);
 			dispatchers.get(msg.guild.id).end();
 		}
@@ -216,7 +231,7 @@ bot.on("message", (msg) => {
 		queue[msg.guild.id] = [];
 
 		msg.channel.sendMessage("Playback stopped.")
-				.then(msg => console.log(`Sent message: ${msg.content}`))
+				.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 				.catch(console.error);
 
 	}
@@ -225,29 +240,33 @@ bot.on("message", (msg) => {
 
 		dispatchers.get(msg.guild.id).once("end", () => { //Called when stream ends
 			queue[msg.guild.id].shift();
+			counter.dec();
 
 			if (queue[msg.guild.id].length > 0) {
 				play(false, queue[msg.guild.id][0][0], connections.get(msg.guild.id)); //Have no idea how this even works
 			} else {
 				msg.channel.sendMessage("No more songs in queue.")
-						.then(msg => console.log(`Sent message: ${msg.content}`))
+						.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 						.catch(console.error);
 
 				connections.get(msg.guild.id).disconnect();
 			}
 
-			console.log("Stream ended");
+			console.log(ct() + "Stream ended");
+		});
+		dispatchers.get(msg.guild.id).once("start", () => {
+			counter.inc();
 		});
 	}
 
 	function play(add, streamurl, connection) { //Play video function
 
-		console.log('Playing stream ' + streamurl);
+		console.log(ct() + 'Playing stream ' + streamurl);
 
 		queueAdd(add, streamurl, () => {
 
 			msg.channel.sendMessage("Now playing: " + queue[msg.guild.id][0][1])
-					.then(msg => console.log(`Sent message: ${msg.content}`))
+					.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 					.catch(console.error);
 
 			const stream = ytdl(streamurl, {filter: 'audioonly'}); //Play :D
@@ -261,19 +280,19 @@ bot.on("message", (msg) => {
 		if (add == true) {
 			ytdl.getInfo(url, (err, info) => {
 				if (err) {
-					console.log(err);
+					console.log(ct() + err);
 					msg.channel.sendMessage("Error adding song, please try again.")
-							.then(msg => console.log(`Sent message: ${msg.content}`))
+							.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 							.catch(console.error);
 					return;
 				}
 				if (queue[msg.guild.id].push([url, info.title + " (" + timestamp(info.length_seconds) + ")"])) {
-					console.log("Added url to queue " + queue[msg.guild.id][0][1]);
+					console.log(ct() + "Added url to queue " + queue[msg.guild.id][0][1]);
 					msg.channel.sendMessage("Song added to queue: " + queue[msg.guild.id][queue[msg.guild.id].length - 1][1])
-							.then(msg => console.log(`Sent message: ${msg.content}`))
+							.then(msg => console.log(ct() + `Sent message: ${msg.content}`))
 							.catch(console.error);
 
-				} else console.log("Error adding song to queue.");
+				} else console.log(ct() + "Error adding song to queue.");
 
 				callback();
 			});
@@ -291,6 +310,18 @@ bot.on("message", (msg) => {
 	
 });
 
+function pad(d) {
+	return (d < 10) ? '0' + d.toString() : d.toString();
+}
 
+function ct() {
+	let date = new Date();
+
+	let h = pad(date.getHours());
+	let m = pad(date.getHours());
+	let s = pad(date.getSeconds());
+
+	return "[" + h + ":" + m + ":" + s + "] "
+}
 
 bot.login('MjY2ODU4MjM4Mzc5NTU2ODg1.C1qwzA.WSqKUdmhBHBEDCJ2QhUUgmhkvRY');
