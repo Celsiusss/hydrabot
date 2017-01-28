0b48545cdb3425431c2892a9ef73132f03acc45e
ende124 <ende124@users.noreply.github.com>
2017-01-28 16:41:58 +0100

    Fix shards

--- manager.js
+++ manager.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
 const log = require('./helpers/log');
 
-const Manager = new Discord.ShardingManager('./bot.js');
+let options = {
+    token: "MjY2ODcyMDQ2OTIxNzExNjE2.C22iow.SwRYS1GRqaiU6HLrWlrat63ZTc4"
+};
+
+const Manager = new Discord.ShardingManager('./bot.js', options);
 
 Manager.on("launch", (shard) => {
     log(`New shard spawned with a total of ${Manager.shards.size}`);
