17da67ed16db1a7cc877822d1c24d0551ec10c77
ende124 <ende124@users.noreply.github.com>
2017-02-18 01:27:05 +0100

    Did some work to webapp

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -4,9 +4,25 @@ const guildsdb = require('../../models/guilds');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	guildsdb.find({guildID: '266859494833651712'}, {}, {lean: true}, (err, docs) => {
-		res.render('index', {title: 'Hello World', ping: docs[0].config.ping});
+	guildsdb.find({guildID: '266859494833651712'}, {}, {lean: true}).then( (doc) => {
+		res.render('index', {title: 'Form Validation', doc: doc, success: req.session.success, errors: req.session.errors});
+		req.session.errors = null;
+		req.session.errors = null;
 	});
+	
+});
+
+router.post('/submit', (req, res, next) => {
+	req.check('email', 'Invalid email address.').isEmail();
+	req.check('password', 'Password is invalid').isLength({min: 6, max: 32}).equals(req.body.cpassword);
+	
+	let errors = req.validationErrors();
+
+	if (errors) {
+		req.session.errors = errors;
+		req.session.success = false;
+	} else req.session.success = true;
+	res.redirect('/');
 });
 
 module.exports = router;
